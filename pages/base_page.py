ddc35bef27457607f1ad82715acd4dad50db742e
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-05-23 15:52:43 +0400

    Сhange code style to PEP8

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,8 @@
-class BasePage():
-def __init__(self, browser, url):
-    self.browser = browser
-    self.url = url
-    
-def open(self):
-    self.browser.get(self.url)
+class BasePage:
+
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
