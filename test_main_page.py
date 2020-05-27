18ccecd95bb3b179a5b24258de0dda8498d53734
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-05-27 19:25:55 +0400

    Сheck for login\registration forms and "login" is in url

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -7,9 +13,23 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
 
+def test_login_or_register_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                     # открываем страницу
+    page.should_be_login_url()      # выполняем метод страницы - переходим на страницу логина
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+
+def test_guest_can_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.should_be_login_form()      # выполняем метод страницы - проверяем, что есть форма логина
+
+def test_guest_can_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_be_register_form()   # выполняем метод страницы - проверяем, что есть форма регистрации
+
+
