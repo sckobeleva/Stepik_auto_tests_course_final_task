82f0a417fa8685baa645ad82b34ce158df1270b0
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-06-02 19:00:08 +0400

    Add linking between pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,22 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 def test_login_or_register_url(browser):
     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
@@ -25,6 +30,7 @@ def test_guest_can_see_login_form(browser):
     page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.should_be_login_form()      # выполняем метод страницы - проверяем, что есть форма логина
+
 
 def test_guest_can_see_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
